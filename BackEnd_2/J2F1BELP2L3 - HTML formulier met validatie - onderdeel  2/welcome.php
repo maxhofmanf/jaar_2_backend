2e728c92536799808ef869c1c77d3f53ffc58cc1
maxhofmanf <99068281@mydavinci.nl>
2023-03-24 08:57:05 +0100

    formulier part 3 done

--- BackEnd_2/J2F1BELP2L3 - HTML formulier met validatie - onderdeel  2/welcome.php
+++ BackEnd_2/J2F1BELP2L3 - HTML formulier met validatie - onderdeel  2/welcome.php
@@ -1,28 +1,32 @@
 <?php
-$email = $name = $required_username = $required_email = ""; 
-
+$email = $name = $required_username = $emailErr = "";
+$required_email = "";
+$emailErr = '';
 
 if (isset($_POST['submit']) && $_SERVER['REQUEST_METHOD'] == "POST") {
-    if(empty($_POST['username'])){
+    if (empty($_POST['username'])) {
         $required_username = "Name is required";
     } else {
         $username = html_check($_POST['username']);
     }
 
-    if(empty($_POST['email'])){
+    if (empty($_POST['email'])) {
         $required_email = "Email is required";
     } else {
         $email = html_check($_POST['email']);
     }
-}
-   function html_check($data){
-    $data = trim( $data );
-    $data = stripslashes( $data );
-    
-    return $data = htmlspecialchars( $data ) ;
-   }
-?>
 
+    if (!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $required_email = "Email is not a valid email address";
+    }
+}
+
+function html_check($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    return $data = htmlspecialchars($data);
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -36,28 +40,23 @@ if (isset($_POST['submit']) && $_SERVER['REQUEST_METHOD'] == "POST") {
 </head>
 
 <body>
-    <?php if(empty( $_POST[ 'username' ]) || empty( $_POST[ 'email' ])){?>
+    <?php if (empty($_POST['username']) || empty($_POST['email']) || $required_email || $required_username) { ?>
 
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
             <label for="username">name:</label><br>
-            <input style="display:inline;" type="text" name="username" ><span style="color:red;">*<?php echo $required_username;?></span><br>
+            <input style="display:inline;" type="text" name="username"><span style="color:red;">*<?php echo $required_username; ?></span><br>
             <label for="email">email:</label><br>
-            <input style="display:inline;" type="text" name="email" ><span style="color:red;">*<?php echo $required_email;?></span>
+            <input style="display:inline;" type="text" name="email"><span style="color:red;">*<?php echo $required_email; ?></span>
             <input type="submit" name="submit">
         </form>
-        <?php } else { ?>
+    <?php } else { ?>
         <h1>De ingevulde gegevens zijn: </h1><br>
         <?php
         echo $username;
-        echo"<br>";
-        echo $email; 
+        echo "<br>";
+        echo $email;
         }
         ?>
-
-
-    
 </body>
 
 </html>
-
-
